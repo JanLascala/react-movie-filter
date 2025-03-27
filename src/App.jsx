b23200a4c78c1b215dbfe2546dc86f5486d9fd7e
JanLascala <lascala.jan@gmail.com>
2025-03-27 17:18:56 +0100

    filter by genre

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,30 @@ function App() {
     { title: 'Interstellar', genre: 'Fantascienza' },
     { title: 'Pulp Fiction', genre: 'Thriller' },
   ]
+
   // by genre
-  const [selectGenre, setSelectgenre] = useState('')
+  const [selectGenre, setSelectgenre] = useState('');
+  const [filteredMovies, setFilteredMovies] = useState(movies);
 
-
-
+  // filter
+  useEffect(() => {
+    if (selectGenre === '') {
+      setFilteredMovies(movies);
+    } else {
+      setFilteredMovies(
+        movies.filter((movie) => movie.genre === selectGenre)
+      );
+    }
+  }, [selectGenre]);
 
   return (
     <>
       <h1>Movie Filter</h1>
 
-      <select name="movies" id="movies"
-        value={(selectGenre)} onChange={(e) => setSelectgenre(e.target.value)}>
-
+      <select
+        value={selectGenre}
+        onChange={(e) => setSelectgenre(e.target.value)}
+      >
         <option value="">Select a genre</option>
         <option value="Fantascienza">Fantascienza</option>
         <option value="Thriller">Thriller</option>
@@ -30,8 +41,15 @@ function App() {
         <option value="Azione">Azione</option>
       </select>
 
+      <ul>
+        {filteredMovies.map((movie) => (
+          <li key={movie.title}>
+            {movie.title} - {movie.genre}
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
 
-export default App
+export default App;
